9dedcbefb1e832e82abbdd3042ce272b586a02bf
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-24 23:59:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+  if (fun === 'roller-skate') {
+    console.log("This Saturday, I want to roller-skate!");
+    return "This Saturday, I want to roller-skate!"
+  } else {
+    console.log(`"This Saturday, I want to ${fun}!"`)
+    return `This Saturday, I want to ${fun}!`
+  }
+}
+
+saturdayFun("bathe my dog")
+
+const mondayWork = function (act = 'go to the office') {
+  if (act === 'go to the office') {
+    console.log("This Monday, I will go to the office.");
+    return "This Monday, I will go to the office.";
+  } else {
+    console.log(`This Monday, I will ${act}.`);
+    return `This Monday, I will ${act}.`
+  }
+}
+
+mondayWork('work from home');
+
+function wrapAdjective( outer ) {
+  if( outer == '*'){
+    const innerFnc = function( inner ){
+      return `You are ${outer}${inner}${outer}!`
+    }
+    return innerFnc('a hard worker')
+  } else if ( outer == '||'){
+    const innerFnc = function( inner ){
+      return `You are ${outer}${inner}${outer}!`
+    }
+    return innerFnc('a dedicated programmer')
+  }
+}
+
+wrapAdjective('*')
